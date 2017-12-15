6a7e98c885611a88fc3275a3139d5feee123303b
mohanprasaths <mohanprasath@live.com>
2017-12-15 13:24:34 +0530

    BST implemented

--- src/main/java/test/datastructures/BST.java
+++ src/main/java/test/datastructures/BST.java
@@ -1,44 +1,68 @@
 package test.datastructures;
 
 public class BST {
-
+    public static void main(String argsp[]){
+        int arr[] = new int[5];
+        arr = new int[]{9,2,1,4,5,0};
+        BinarySearchTree bstree = new BinarySearchTree();
+        bstree.buildTree(arr);
+        bstree.displayTree(bstree.root);
+    }
 }
 
 class BinarySearchTree{
     Node root;
-    BinarySearchTree(int arr[]){
+    BinarySearchTree(){
         this.root = null;
     }
-    insertIntoTree(int value,Node currentNode){
+    void buildTree(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            insertIntoTree(arr[i], this.root);
+        }
+    }
+    void insertIntoTree(int value,Node currentNode){
+        Node newNode = this.findPreNode(value,currentNode);
+        newNode.setNode(value);
+    }
+    Node findPreNode(int value,Node currentNode){
         if(this.root == null){
-            this.root.value = value;
-            return;
+            this.root = new Node(value);
+            return this.root;
         }
-        if(value > currentNode.value){
-            if(currentNode.right == null){
-                currentNode.right = new Node(value);
-            }else{
-                this.insertIntoTree(value,currentNode.right);
-            }
-            return;
-        }
-        if(value < currentNode.value){
+        if(currentNode.value > value){
             if(currentNode.left == null){
                 currentNode.left = new Node(value);
-            }else{
-                this.insertIntoTree(value,currentNode.left);
+                return currentNode.left;
             }
-            return;
+            return findPreNode(value , currentNode.left);
+        }else{
+           if(currentNode.right == null){
+               currentNode.right= new Node(value);
+               return currentNode.right;
+           }
+           return findPreNode(value , currentNode.right);
+        }
+    }
+    void displayTree(Node root){
+        if(root != null){
+         System.out.println(root.value);
+         displayTree(root.left);
+         displayTree(root.right);
         }
     }
 }
 
 
 class Node {
-    int value;
+    public int value;
     Node left;
     Node right;
     Node(int value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+    void setNode(int value){
         this.value = value;
         this.left = null;
         this.right = null;
